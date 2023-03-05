7152d7378152143422c0230b98bd104222fdd0e9
Jazz <JazzDevours@gmail.com>
2023-03-05 15:44:37 -0500

    add calculator buttons with styling

--- javascript.js
+++ javascript.js
@@ -28,3 +28,22 @@ function operate(operator, currentVal, newVal) {
     return multiply(currentVal, newVal);
   }
 }
+
+const container = document.querySelector('#container');
+const buttons = document.querySelector('#buttons');
+
+function createCalc(x, y) {
+  for (let y = 0; y < gridSize; y++) {
+    const div = document.createElement('div');
+    div.classList.add('coloumn');
+    container.appendChild(div);
+    for (let x = 0; x < gridSize; x++) {
+    const button = document.createElement('button');
+    button.classList.add('btn');
+    btn.addEventListener('click', selectButton);
+      divx.appendChild(divy)
+    }
+  }
+}
+
+createCalc(4, 5);
